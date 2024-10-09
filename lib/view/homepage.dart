ad64aa06b0b1c047b32a1882290799d141e93749
devv-thiago <tfofano3@gmail.com>
2024-10-09 19:59:21 -0300

    iniciando desenvolvimento layout homepage

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:urban_green/controller/cliente.dart';
+import 'package:urban_green/shared/style/colors.dart';
 import 'package:urban_green/shared/style/components/pedido_widget.dart';
 
 class Homepage extends StatelessWidget {
@@ -10,20 +11,54 @@ class Homepage extends StatelessWidget {
     MediaQueryData deviceInfo = MediaQuery.of(context);
     ClienteController clienteController = ClienteController();
 
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: SizedBox(
-        height: deviceInfo.size.height * 0.8,
-        width: deviceInfo.size.width,
-        child: ListView.builder(
-          itemCount: clienteController.pedidos.length,
-          itemBuilder: (context, index) => PedidoWidget(
-            deviceInfo.size.height * 0.2,
-            deviceInfo.size.width * 0.7,
-            clienteController.pedidos[index],
-          ),
+    return Scaffold(
+      body: Stack(alignment: Alignment.bottomCenter, children: <Widget>[
+        Container(
+          height: deviceInfo.size.height,
+          width: deviceInfo.size.width,
+          color: AppColors.color1,
+          child: SingleChildScrollView(
+              child: Column(
+            children: [
+              SizedBox(
+                height: deviceInfo.size.height * 0.3,
+                child: Image.asset('assets/images/app_icon_white.png'),
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                    left: deviceInfo.size.width * 0.1,
+                    right: deviceInfo.size.width * 0.1,
+                    bottom: deviceInfo.size.height * 0.1),
+                height: deviceInfo.size.height * 0.7,
+                width: deviceInfo.size.width,
+                decoration: const BoxDecoration(
+                    color: AppColors.color3,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    )),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: deviceInfo.size.height * 0.7,
+                      width: deviceInfo.size.width,
+                      child: ListView.builder(
+                        itemCount: clienteController.pedidos.length,
+                        itemBuilder: (context, index) => PedidoWidget(
+                          deviceInfo.size.height * 0.2,
+                          deviceInfo.size.width * 0.7,
+                          clienteController.pedidos[index],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          )),
         ),
-      ),
+      ]),
     );
   }
 }
